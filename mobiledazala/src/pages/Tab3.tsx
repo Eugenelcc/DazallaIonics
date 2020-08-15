6d21b5641ccc0c844304f6de343d2f33fb607419
Eugenelcc <Eugenelcc@github.com>
2020-08-15 19:17:49 +0800

    Update Tab3.tsx
    Update till part 3 of the assignment

--- mobiledazala/src/pages/Tab3.tsx
+++ mobiledazala/src/pages/Tab3.tsx
@@ -1,22 +1,21 @@
 import React from 'react';
 import { useState, useEffect, useReducer } from "react";
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonItem, IonLabel, IonLoading, IonList, IonTextarea,  IonGrid, IonRow, IonCol, IonButton } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonItem, IonLabel, IonLoading, IonList, IonTextarea, IonGrid, IonRow, IonCol, IonButton } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab3.css';
 import { RouteComponentProps } from 'react-router';
 import useLocalStorage from '../components/LocalStorage'
-import { cartOutline } from 'ionicons/icons';
 
 interface CartPageProps extends RouteComponentProps<{
   id: string;
 }> { }
 
-interface CartItem { 
+interface CartItem {
   id: string
   name?: string
   description?: string
   price?: number
-  qty:number
+  qty: number
 }
 
 
@@ -24,51 +23,46 @@ interface Dictionary<T> {
   [Key: string]: T;
 }
 
-type Cart = Dictionary<CartItem> //Cart = Dictionary 
+type Cart = Dictionary<CartItem>
 
-  
 
-const addItem = async function (id: string, cart: Cart): Promise<Cart>{// Eugene Lay 
-  
+
+
+const addItem = async function (id: string, cart: Cart): Promise<Cart> { // TODO:fixme 
   if ((id != null) && (id.length > 0)) {
-    if ( cart[id] == null) {
-      cart[id] = { id:id, qty:1 };
+    if (cart[id] == null) {
+      cart[id] = { id: id, qty: 1 };
     } else {
-      cart[id].qty += 1 
+      cart[id].qty += 1
     }
   }
   return new Promise((resolve, reject) => {
-    resolve (cart)  
+    resolve(cart)
   })
 }
 
-function allItems(cart: Cart): CartItem[] { // Eugene Lay 
+
+function allItems(cart: Cart): CartItem[] {
   let res = new Array<CartItem>()
-  for (const items of Object.keys(cart)) {
-    res.push(cart[items]);
+  for (const item of Object.keys(cart)) {
+    res.push(cart[item]);
   }
-  return res;
+  return res
 }
 
-const Tab3: React.FC<CartPageProps> = ({match}) => {
-  const [showLoading, setShowLoading] = useState<boolean>(true);
-  const [ cart, setCart  ] = useLocalStorage<Cart>('cart', {});
 
-  const fetchProducts = async () => {// Eugene Lay 
+const Tab3: React.FC<CartPageProps> = ({ match }) => {
+  const [showLoading, setShowLoading] = useState<boolean>(true);
+  const [cart, setCart] = useLocalStorage<Cart>('cart', {});
+
+
+  useEffect(() => {
     fetch(`https://localhost:5001/api/product/get/${match.params.id}`)
-      .then(res => res.json())
+    
+      addItem(match.params.id, cart)
       .then(setCart)
       .finally(() => setShowLoading(false))
-  }
-
-
-  useEffect(() => { 
-    fetchProducts()
-
-    
-
-    
-  }, [cart])
+  }, [])
 
   return (
     <IonPage>
@@ -89,7 +83,17 @@ const Tab3: React.FC<CartPageProps> = ({match}) => {
           message={'Loading...'}
         />
         <IonList>
-         {}
+          {allItems(cart).map((item, index) => (
+            <IonItem key={index}>
+              <IonLabel>
+                <p>Product ID :{item.id}</p>
+                <h1>{item.name}</h1>
+                <p>{item.description}</p>
+                <p>{item.price}</p>
+                <p>Item quantity:{item.qty}</p>
+              </IonLabel>
+            </IonItem>
+          ))}
         </IonList>
       </IonContent>
     </IonPage>
